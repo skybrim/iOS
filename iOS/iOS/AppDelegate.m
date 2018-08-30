c554f6741716defeb0b39cab10f4d487c9cbc9ea
Skybrim <throughskybrim@gmail.com>
2018-08-30 15:41:33 +0800

    创建develop分支

--- iOS/iOS/AppDelegate.m
+++ iOS/iOS/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"master");
+    NSLog(@"develop");
     return YES;
 }
 
