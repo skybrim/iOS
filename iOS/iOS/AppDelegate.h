7ed976a4c9a0cb3158228ca66dfac78e8d7504d4
Skybrim <throughskybrim@gmail.com>
2018-08-30 15:40:11 +0800

    创建工程

---
+++ iOS/iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS
+//
+//  Created by mac on 2018/8/30.
+//  Copyright © 2018年 Mr.T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
