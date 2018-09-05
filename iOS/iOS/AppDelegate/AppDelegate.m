10f63ea22941a7fe837aeade53d1365a76138df4
Skybrim <throughskybrim@gmail.com>
2018-09-05 15:37:10 +0800

    测试合并

--- iOS/iOS/AppDelegate/AppDelegate.m
+++ iOS/iOS/AppDelegate/AppDelegate.m
@@ -18,8 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"develop");
-    
+    NSLog(@"master");
+
     return YES;
 }
 
