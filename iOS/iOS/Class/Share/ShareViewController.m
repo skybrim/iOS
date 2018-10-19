1cf89b88bcb2542188f79651e00ffa7db28519e8
Skybrim <throughskybrim@gmail.com>
2018-10-19 16:53:22 +0800

    分享初始，开始优化

--- iOS/iOS/Class/Share/ShareViewController.m
+++ iOS/iOS/Class/Share/ShareViewController.m
@@ -8,7 +8,11 @@
 
 #import "ShareViewController.h"
 
+#import "KMPShareHandle.h"
+
 @interface ShareViewController ()
+
+@property (nonatomic,strong) UIButton *shareBtn;
 
 @end
 
@@ -17,6 +21,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self layoutPages];
+}
+
+#pragma mark - layout
+- (void)layoutPages {
+    
+    self.title = @"分享";
+    
+    [self.view addSubview:self.shareBtn];
+    [self.shareBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(@0);
+        make.centerY.equalTo(@-100);
+    }];
+}
+
+#pragma mark - getter
+- (UIButton *)shareBtn {
+    
+    if (!_shareBtn) {
+        
+        _shareBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        _shareBtn.backgroundColor = [UIColor cyanColor];
+        [_shareBtn setTitle:@"分享" forState:UIControlStateNormal];
+        [_shareBtn sizeToFit];
+        
+        [_shareBtn bk_whenTapped:^{
+           
+            [KMPShareHandle sharedKMPShareHandle].shareTitle = @"shareTitle";
+            //内容在30字以内
+            [KMPShareHandle sharedKMPShareHandle].shareContent = @"shareContent";
+            [KMPShareHandle sharedKMPShareHandle].shareImage = [UIImage imageNamed:@"placeholder"];
+            [KMPShareHandle sharedKMPShareHandle].shareUrl = @"https://www.baidu.com";
+            [[KMPShareHandle sharedKMPShareHandle] showWithVC:self];
+            
+            [[KMPShareHandle sharedKMPShareHandle] setSuccessBlock:^{
+                
+                NSLog(@"share-success");
+            }];
+            
+            [[KMPShareHandle sharedKMPShareHandle] setFailureBlock:^(NSError *error) {
+                
+                NSLog(@"%@",error);
+            }];
+        }];
+    }
+    return _shareBtn;
 }
 
 /*
